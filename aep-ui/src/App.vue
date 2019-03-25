2ec6d0e5a4e285a7170a2156c44f1dca48296978
Nagarajan Sivaraman <naga4042@gmail.com>
2019-03-25 19:41:50 +0530

    adding bootstrap

--- aep-ui/src/App.vue
+++ aep-ui/src/App.vue
@@ -1,29 +1,35 @@
 <template>
+
   <div id="app">
-    <div id="nav">
+    
+
+    <nav id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/about">API Hackathon</router-link>
+    </nav>
+    
     <router-view />
+
+    <footer>
+      &copy; Software AG, 2019.
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+nav {
+  position: fixed;
+  top: 0;
+  height: 50px;
+  width: 100%;
+  background-color: $blue;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+footer {
+  position: fixed;
+  bottom: 0;
 }
 </style>
